9bc7c06beba1dd2cc7133d5df6b8cd05ac732000
TibaAlH <s2300781@edu.bc.fi>
2025-01-30 19:29:56 +0200

    Tehtävä 3.3 - Nuolelle ominaisuudet

--- Nuolikauppa/Nuoli.cs
+++ Nuolikauppa/Nuoli.cs
@@ -16,32 +16,33 @@
 
     internal class Nuoli
     {
-        private sbyte pituusCm;
-        private SulkaMateriaali Sulka;
-        private KärkiMateriaali Kärki;
+        public sbyte pituusCm { get; set; }
+        public SulkaMateriaali sulka { get; set; }
+        public KärkiMateriaali kärki { get; set; }
+        
 
         // tee konstruktori
         public Nuoli(KärkiMateriaali kärki, SulkaMateriaali sulka, float pituusCm)
         {
 
-            this.Kärki = kärki;
-            this.Sulka = sulka;
+            this.kärki = kärki;
+            this.sulka = sulka;
             this.pituusCm = (sbyte)pituusCm;
         }
 
         public sbyte GetPituus() => pituusCm;
-        public SulkaMateriaali GetSulka() => Sulka;
-        public KärkiMateriaali GetKärki() => Kärki;
+        public SulkaMateriaali GetSulka() => sulka;
+        public KärkiMateriaali GetKärki() => kärki;
         public float AnnaHinta()
         {
             float hinta = 0;
-            hinta += Kärki switch
+            hinta += kärki switch
             {
                 KärkiMateriaali.Puu => 3,
                 KärkiMateriaali.Teräs => 5,
                 KärkiMateriaali.Timantti => 50,
             };
-            hinta += Sulka switch
+            hinta += sulka switch
             {
                 SulkaMateriaali.Kanansulka => 1,
                 SulkaMateriaali.Kotkansulka => 5,
