a30a5d99b88cf38f0635c6116a12a23027c519de
TibaAlH <s2300781@edu.bc.fi>
2025-01-31 11:47:18 +0200

    Complete: Suorakulmioiden piirtäminen

---
+++ Suorakulmio/Suorakulmioiden piirtäminen/Program.cs
@@ -0,0 +1,35 @@
+﻿using Raylib_cs;
+using System.Numerics;
+namespace Suorakulmioiden_piirtäminen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector2 pointPosition = new Vector2(220, 170);
+            int screenWidth = 600;
+            int screenHeight = 400;
+            Raylib.InitWindow(screenWidth, screenHeight, "Raylib");
+            while (Raylib.WindowShouldClose() == false)
+            {
+                Rectangle collisionTest = new Rectangle(200, 150, 60, 60);
+                Raylib.DrawRectangleRec(collisionTest, Color.Blue);
+
+                Vector2 position = new Vector2(200, 150);
+                Vector2 size = new Vector2(60, 80);
+
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Black);
+                Raylib.DrawRectangleV(position, size, Color.Blue);
+
+                Raylib.DrawCircle(220, 170 , 6, Color.White);
+                if (Raylib.CheckCollisionPointRec(pointPosition, collisionTest))
+                {
+                    Raylib.DrawText("Collision!",220, 60, 32, Color.White);
+                }
+
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
